7de3c2b85fed52aa29648a90d03f032f9592a3a3
rajesh <rajeshupadhayaya68@gmail.com>
2023-12-16 18:45:06 -0700

    Sum of digit in N recursively.

---
+++ recursion/sum_digit.py
@@ -0,0 +1,17 @@
+def sum_digit(n):
+    """
+    Returns the sum of digit on N
+    Time Complexity: O(d), where d is the number of digits in n.
+    Space Complexity: O(d), where d is the number of digits in n.
+    :param n: Positive integer number
+    :return: Sum of digits in n
+    """
+    if int(n / 10) < 1:
+        return n
+    return (n % 10) + sum_digit(int(n / 10))
+
+
+if __name__ == '__main__':
+    print(sum_digit(253))
+    print(sum_digit(10))
+    print(sum_digit(9987))
