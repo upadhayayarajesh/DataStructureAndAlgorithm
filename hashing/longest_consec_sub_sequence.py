440367aea0e46f2257c73f982a79ab056b285c3d
rajesh <rajeshupadhayaya68@gmail.com>
2024-01-22 18:45:53 -0700

    Effective approach to find longes consecutive sub seq

--- hashing/longest_consec_sub_sequence.py
+++ hashing/longest_consec_sub_sequence.py
@@ -21,9 +21,26 @@ class LongestConsecSubSequence:
                 cons = 1
         return res
 
+    def longest_consecutive_sub_seq_eff(self, nums1):
+        hash = {}
+        res = 1
+        for num in nums1:
+            hash[num] = num
+
+        for v in nums1:
+            if v - 1 in hash.keys():
+                continue
+            else:
+                count = 1
+                while v + 1 in hash.keys():
+                    count += 1
+                    v += 1
+                res = max(res, count)
+        return res
+
 
 if __name__ == '__main__':
     test_arr_1 = [1, 9, 3, 4, 2, 10]
     test_arr_2 = [8, 20, 7, 30]
     print(LongestConsecSubSequence().longest_consec_sub_sequence(test_arr_1))
-    print(LongestConsecSubSequence().longest_consec_sub_sequence(test_arr_2))
+    print(LongestConsecSubSequence().longest_consecutive_sub_seq_eff(test_arr_2))
