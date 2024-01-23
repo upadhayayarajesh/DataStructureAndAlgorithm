7fec21695358e78e23b07121dc083e96a88d391c
rajesh <rajeshupadhayaya68@gmail.com>
2024-01-23 13:47:32 -0700

    Application of linked list.

---
+++ linked_link/README.md
@@ -0,0 +1,13 @@
+# LINKED LIST
+
+## Application of Linked List
+
++ worst case insertion at the end and begin are &#952;(1)
++ Worst case deletion from the beginning &theta;(1)
++ Insertion and deletions in the middle are &theta;(1) if we have reference to the previous node.
++ Round Robin Implementation
++ Merging two sorted linked  lists is faster than arrays.
++ Implementation of simple memory manager where we need to link free blocks.
++ Easier implementation of queue and Dequeue data structures.
+
+
---
+++ linked_link/linked_list_implementation.py
@@ -0,0 +1,67 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    """
+    A linked list implementation in python
+    """
+
+    def __init__(self):
+        self.head = Node()
+        self.size = 0
+
+    def append(self, data):
+        if self.size == 0:
+            self.head.data = data
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            new_node = Node(data)
+            current.next = new_node
+        self.size += 1
+
+    def get_size(self):
+        return self.size
+
+    def check(self, data):
+        if self.size == 0:
+            return False
+        else:
+            current = self.head
+            while current is not None:
+                if current.data == data:
+                    return True
+                current = current.next
+
+    def remove(self, data):
+        if self.check(data):
+            current = self.head
+            while current is not None:
+                if current.data == data:
+                    current.data = current.next.data
+                    break
+                current = current.next
+        else:
+            print("Not found")
+            return False
+
+    def display(self):
+        display = ""
+        current = self.head
+        while current is not None:
+            display += str(current.data) + " "
+            current = current.next
+        print(display)
+
+
+if __name__ == '__main__':
+    linked_list = LinkedList()
+    linked_list.append(5)
+    linked_list.append(10)
+    linked_list.append(30)
+    linked_list.append(50)
+    linked_list.display()
