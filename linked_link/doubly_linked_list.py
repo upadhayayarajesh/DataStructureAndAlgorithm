bbf48c7e64b22fa756b5ec01d70813eb66f59f01
rajesh <rajeshupadhayaya68@gmail.com>
2024-01-23 15:54:02 -0700

    Insert at begining.

--- linked_link/doubly_linked_list.py
+++ linked_link/doubly_linked_list.py
@@ -9,7 +9,39 @@ class DoublyLinkedList:
 
     def __init__(self):
         self.head = Node()
+        self.size = 0
+
+    def insert_begin(self, data):
+        """
+        Time Complexity: O(1)
+        """
+        if not self.head.data:
+            self.head.data = data
+        else:
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.size += 1
+
+    def display(self):
+        """
+        Time Complexity: O(n)
+        """
+        res = ""
+        if self.size == 0:
+            print(res)
+        else:
+            curr = self.head
+            while curr:
+                res += str(curr.data) + " "
+                curr = curr.next
+        print(res)
 
 
 if __name__ == '__main__':
     doubly_linked_list = DoublyLinkedList()
+    doubly_linked_list.insert_begin(3)
+    doubly_linked_list.insert_begin(2)
+    doubly_linked_list.insert_begin(4)
+    doubly_linked_list.display()
