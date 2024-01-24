c04cf74b36399861e2d1e18b8b5a0294a32283c9
rajesh <rajeshupadhayaya68@gmail.com>
2024-01-23 21:02:27 -0700

    Insert at the end.

--- linked_link/circular_link_list.py
+++ linked_link/circular_link_list.py
@@ -20,6 +20,20 @@ class Circular_linked_list:
             self.head.next = new_node
             self.head.next.data = self.head.data
             self.head.data = data
+            self.size += 1
+
+    def insert_tail(self, data):
+        head = self.head
+        if self.size == 0:
+            self.head.data = data
+            self.size += 1
+        else:
+            curr = self.head
+            while curr.next.data != head.data:
+                curr = curr.next
+            new_node = Node(data)
+            new_node.next = self.head
+            curr.next = new_node
 
     def display(self):
         if self.size == 0:
@@ -39,4 +53,5 @@ if __name__ == "__main__":
     cll.insert_head(10)
     cll.insert_head(30)
     cll.insert_head(20)
+    cll.insert_tail(40)
     cll.display()
