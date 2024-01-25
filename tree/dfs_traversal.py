f04474f6373c16f401797998e184d028e993778f
rajesh <rajeshupadhayaya68@gmail.com>
2024-01-25 08:12:29 -0700

    Preorder and Postorder traversal of the tree.

--- tree/dfs_traversal.py
+++ tree/dfs_traversal.py
@@ -17,10 +17,27 @@ class DFS_Traversal:
         """
         Time Complexity: O(N)
         Space Complexity: O(h), where h is the height of the tree.
-        :param root:
-        :return:
         """
         if root is not None:
             self.inorder_traversal(root.left)
             print(root.data)
             self.inorder_traversal(root.right)
+
+    def preorder_traversal(self, root):
+        """
+        Time Complexity: O(N)
+        Space Complexity: O(h), where h is the height of the tree.
+        """
+        if root is not None:
+            print(root)
+            self.preorder_traversal(root.left)
+            self.preorder_traversal(root.right)
+
+    def postorder_traversal(self, root):
+        """
+        Time Complexity: O(N)
+        Space Complexity: O(h), where h is the height of the tree.
+        """
+        if root is not None:
+            self.postorder_traversal(root.right)
+            print(root)
