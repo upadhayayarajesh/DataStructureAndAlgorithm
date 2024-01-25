55678f5330dc075f73fa8cffbe885cdce44f7703
rajesh <rajeshupadhayaya68@gmail.com>
2024-01-25 10:32:30 -0700

    Line by line traversal for bfs

--- tree/bfs_traversal.py
+++ tree/bfs_traversal.py
@@ -18,20 +18,27 @@ class BFS_Traversal:
 
     def bfs_traversal(self, root):
         """
-        Time Complexity: O(n)
-        Space Complexity: O(n)
+        Time Complexity: O(n + h), h is the height of the tree
+        Space Complexity: O(w) , w is the width of the tree
         """
         if root.data is None:
             return
+        res = ""
         queue = Queue()
         queue.put(root)
-        while not queue.empty():
+        queue.put(None)
+        while queue.qsize() > 1:
             node = queue.get()
-            print(node.data)
+            if node is None:
+                res += "\n"
+                queue.put(None)
+                continue
+            res += str(node.data) + " "
             if node.left is not None:
                 queue.put(node.left)
             if node.right is not None:
                 queue.put(node.right)
+        print(res)
 
 
 if __name__ == '__main__':
