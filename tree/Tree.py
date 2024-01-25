3e867ebd5f29681db09cf19c816d81ece2bbbf33
rajesh <rajeshupadhayaya68@gmail.com>
2024-01-25 12:53:36 -0700

    Maximum in the tree using recursive.

--- tree/Tree.py
+++ tree/Tree.py
@@ -61,6 +61,16 @@ class Tree:
         else:
             return 1 + self.tree_size(root.left) + self.tree_size(root.right)
 
+    def tree_maximum(self, root):
+        """
+        Time Complexity: O(N)
+        Space Complexity: O(h), where h is the height of the tree
+        """
+        if root is None:
+            return float('-inf')
+        else:
+            return max(root.data, self.tree_maximum(root.left), self.tree_maximum(root.right))
+
 
 if __name__ == '__main__':
     tree = Tree()
@@ -69,3 +79,4 @@ if __name__ == '__main__':
     print(tree.height(root))
     tree.node_distance(root, 2)
     print(tree.tree_size(root))
+    print(tree.tree_maximum(root))
